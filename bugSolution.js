fd3dd1426259a03554c681c03b38467b47376467
BugHunter-X <askzenai@gmail.com>
2025-01-12 04:26:39 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+//Solution using a custom CSS class with Tailwind's @apply directive
+<div class="w-full pl-4 pr-4">
+  Some content
+</div>
+
+/* Or in your main CSS file */
+.my-calc-width { @apply w-full pl-4 pr-4; }
+```
